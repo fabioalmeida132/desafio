8fbbc320b203c2554e80bd88bd5c9f6a70f06895
Fábio <fabioalmeida132@gmail.com>
2022-03-31 23:24:07 -0300

    update route

--- start/routes/index.ts
+++ start/routes/index.ts
@@ -1,4 +1,12 @@
+import Route from '@ioc:Adonis/Core/Route'
+
 import './boards'
 import './tasks'
 import './subtasks'
 import './status'
+
+Route.get('/', ({ response }) => {
+  response.send({
+    message: 'API is running'
+  })
+})
